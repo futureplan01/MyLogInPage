9b4e6a0c08e44a3c818d8afc4877b57b4cbb65f4
Jesaiah Prayor <jesaiahprayor@gmail.com>
2017-07-09 07:29:00 -0400

    removed all unused files

--- post.js
+++ post.js
@@ -1,17 +1,24 @@
 var firstButton = document.getElementsByTagName("button")[0];
 var firstTextBox = document.getElementsByTagName("input")[0];
 var secondTextBox = document.getElementsByClassName("box")[1];
-var mysql_newuser = require("./register.js");
+// var register = require("./register.js");
 
 
+// if the fil
 firstButton.onclick = function (err){
-	var user = firstTextBox.value;
+	var username = firstTextBox.value;
 	var password = secondTextBox.value;
-	if(user === "" || password = ""){
+	if(username === "" || password === ""){
 		alert("Please Enter A Email Address and Password");
 	}else{
+		var buttonName = firstButton.name;
+		if(buttonName === "Log in"){
+			// register.checkDatabase(username,password);
+
 			
-		// Get into the index.js file
-		window.location.href = "https://www.google.com.jm/?gfe_rd=cr&ei=OkhdWc66COydXqS2mLAE";
+		}else{
+			// register.insert(username,password);
+			
+		}
 	}
 }
--- temp.js
+++ temp.js
@@ -1,3 +1,1 @@
-var mysql = require("./temp1.js");
 
-console.log(mysql.big); 
