bd6cdbd277665b6276eefa5dd3aa7817bbd81475
Jesaiah Prayor <jesaiahprayor@gmail.com>
2017-07-10 13:52:59 -0400

    made an express server

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+var path = require('path');
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+app.use(express.static("views"))
+app.get('/',function (req,res) {
+	//res.send("WHats uppppppp"); 
+	res.render('index', {user : "Great User"});
+});
+app.listen (port,function (err) {
+	if(err){
+		return console.log("no bueno");
+	}
+	console.log(`server is listening on ${port}`);
+})
