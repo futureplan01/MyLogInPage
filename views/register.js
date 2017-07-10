27822cd2b332cbe27da6d32f9c6ea1967070cb8e
Jesaiah Prayor <jesaiahprayor@gmail.com>
2017-07-10 14:17:05 -0400

    working on mysql

---
+++ views/register.js
@@ -0,0 +1,14 @@
+module.exports = {
+	var mysql = require ("mysql");
+	var con = mysql.createConnection ({
+		host: "127.0.0.1";
+		user: "root";
+		password: "jays101";
+	});
+
+	checkDatabase: function (username, password){
+		
+	}
+
+
+}
