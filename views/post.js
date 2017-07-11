1c7048381e7f50997abe7a7564f175115fa1d783
Jesaiah Prayor <jesaiahprayor@gmail.com>
2017-07-11 08:36:42 -0400

    Up to date

--- views/post.js
+++ views/post.js
@@ -1,7 +1,7 @@
 var firstButton = document.getElementsByTagName("button")[0];
 var firstTextBox = document.getElementsByTagName("input")[0];
 var secondTextBox = document.getElementsByClassName("box")[1];
-// var register = require("./register.js");
+var register = require("./register.js");
 
 
 // if the fil
@@ -14,8 +14,10 @@ firstButton.onclick = function (err){
 		var buttonName = firstButton.name;
 		if(buttonName === "Log in"){
 			register.checkDatabase(username,password);
+
 		}else{
 			register.insert(username,password);
+			alert("Yo");
 		}
 	}
 }
--- views/register.js
+++ views/register.js
@@ -24,6 +24,7 @@ module.exports = {
 	},
 
 	insert: function (username, password){
+		console.log("YOOO");
 		con.connect (function(err){
 			if (err) throw err;
 			console.log("Connected!");
